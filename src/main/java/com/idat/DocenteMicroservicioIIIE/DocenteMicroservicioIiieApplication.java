77bc875d0a2012b92af44ab4eafc2b61ad3152de
Jesus Toledo <jesusplasencia2508@hotmail.com>
2022-11-22 21:36:11 -0500

    Practica Microservice Docente

---
+++ src/main/java/com/idat/DocenteMicroservicioIIIE/DocenteMicroservicioIiieApplication.java
@@ -0,0 +1,13 @@
+package com.idat.DocenteMicroservicioIIIE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocenteMicroservicioIiieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocenteMicroservicioIiieApplication.class, args);
+	}
+
+}
